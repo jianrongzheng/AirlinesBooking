1016099dd9f176e217aee505746384542806053d
Jianrong Zheng <u1573041@unimail.hud.ac.uk>
2016-04-17 04:59:53 +0800

    Login area and say hello to user function

---
+++ Default.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            getTInfoList();
+            if (Session["Users"] == null)
+            {
+                TABLE1.Visible = true;
+                table2.Visible = false;
+            }
+            else
+            {
+                TABLE1.Visible = false;
+                table2.Visible = true;
+                MName.Text = Session["Users"].ToString();
+            }
+        }
+
+    }
+   
+}
