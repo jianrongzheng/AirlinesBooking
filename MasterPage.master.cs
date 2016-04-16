0f762442b570c2f827beb37313081aa240f75e5d
Jianrong Zheng <u1573041@unimail.hud.ac.uk>
2016-04-16 19:23:01 +0800

    Add function to Label to jump to administrator login page

---
+++ MasterPage.master.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.Data.SqlClient;
+public partial class MasterPage : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            Get();
+        }
+
+    }
+    private void Get()
+    {
+
+    }
+    protected void lbtnALogin_Click(object sender, EventArgs e)
+    {
+        Response.Write("<script language=javascript>window.open('AdminManage/Login.aspx','','width=455,height=255')</script>");
+    }
+}
