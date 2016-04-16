1d3a63ade88ded6de39c0f5eb75c217e8837fc1f
Jianrong Zheng <u1573041@unimail.hud.ac.uk>
2016-04-17 05:02:45 +0800

    Log in function and connect database confirm ID and password

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -30,5 +30,23 @@ public partial class _Default : System.Web.UI.Page
         }
 
     }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        AirTicketWeb.BLL.Member login = new AirTicketWeb.BLL.Member();
+        if (login.UserLogin(txtMName.Text, txtMPwd.Text))
+        {
+            Session["Users"] = txtMName.Text;
+
+            TABLE1.Visible = false;
+            table2.Visible = true;
+            MName.Text = txtMName.Text;
+        }
+        else
+        {
+            TABLE1.Visible = true;
+            table2.Visible = false;
+            Label1.Text = "Error.Please be aware that your ID/Password are case sensitive.Please check and re-enter.";
+        }
+    }
    
 }
