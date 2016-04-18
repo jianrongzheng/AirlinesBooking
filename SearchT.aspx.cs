58639992f586b21ad5fd80f3a76ec82b24a30032
Jianrong Zheng <u1573041@unimail.hud.ac.uk>
2016-04-19 05:50:42 +0800

    Function to jump to result list when click search botton

---
+++ SearchT.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+
+public partial class SearchT : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    
+    //click search botton then jump to the result list, call "SearchList"
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("SearchList.aspx?StartT=" + TextBox2.Text + "&ET=" + TextBox1.Text + "&STime="+TextBox3.Text);
+    }
+    protected void TextBox3_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+}
