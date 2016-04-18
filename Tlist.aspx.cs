d6f12f98e7e6408ac3fc0e03468b859c3c529dd6
Jianrong Zheng <u1573041@unimail.hud.ac.uk>
2016-04-19 06:34:22 +0800

    Connect to database show the airlines

---
+++ Tlist.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.Data.SqlClient;
+public partial class Tlist : System.Web.UI.Page
+{
+    //Get Airlines information when this page first execute
+    protected void Page_Load(object sender, EventArgs e) 
+    {
+        if (!IsPostBack)
+        {
+            getTInfoList();
+        }
+    }
+    
+    private void getTInfoList()
+    {
+        try
+        {
+            //Page control
+            DataSet ds = new DataSet();
+
+            AirTicketWeb.BLL.TInfo GetTInfo = new AirTicketWeb.BLL.TInfo();
+            ds = GetTInfo.GetAllList();
+
+            PagedDataSource objPds = new PagedDataSource();
+            objPds.DataSource = ds.Tables[0].DefaultView;
+
+            objPds.AllowPaging = true;
+            objPds.PageSize = 12;
+
+            int CurPage;
+            if (Request.QueryString["Page"] != null)
+                CurPage = Convert.ToInt32(Request.QueryString["Page"]);
+            else
+                CurPage = 1;
+
+            objPds.CurrentPageIndex = CurPage - 1;
+            lblCurrentPage.Text = CurPage.ToString();
+            lblSumPage.Text = objPds.PageCount.ToString();
+
+
+            DataList1.DataSource = objPds;//Connect to database show the information
+            DataList1.DataBind();
+
+
+        }
+        catch
+        {
+
+        }
+
+    }
+}
